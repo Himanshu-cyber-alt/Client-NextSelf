f6ed0504c68dfd51a740bfc9fbd38aa28d0d9516
Himanshu Pagare <155243085+Himanshu-cyber-alt@users.noreply.github.com>
2026-07-31 19:54:01 +0530

    Fix

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -345,7 +345,7 @@ import {
 } from "../services/authService";
 
 import { FaStudiovinari } from "react-icons/fa";
-const DURATION = 1 * 60;
+const DURATION = 0.1 * 60;
 
 const audio = new Audio("/done.mp3");
 
