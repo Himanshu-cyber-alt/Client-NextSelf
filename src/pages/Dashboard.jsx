3f66e2298469890febe78a36f0d48e8e35f33180
Himanshu Pagare <155243085+Himanshu-cyber-alt@users.noreply.github.com>
2026-09-02 21:03:47 +0530

    fix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -551,10 +551,7 @@ export default function Dashboard() {
       try {
         const response = await checkFocusStatus(uuid);
 
-        if (response.is_running) {
-          alert("Finish your current focus session first!");
-          return;
-        }
+     
 
         // 1. Setup Audio
         if (!startAudio.current) {
